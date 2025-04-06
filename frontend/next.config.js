8646613c1c35d3ee7313817e08f7fbca162266fe
purange <jumokcom2001@gmail.com>
2025-04-06 10:49:05 +0900

    Clean up configuration files and update build settings

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
 })
 
 /** @type {import('next').NextConfig} */
